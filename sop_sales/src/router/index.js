50a7e06b69b1cb7e67097ea34b37dbcb5d1d8f97
IJerickoZ <piya.pkk@outlook.com>
2021-12-17 00:43:04 +0700

    Update index.js

--- sop_sales/src/router/index.js
+++ sop_sales/src/router/index.js
@@ -1,3 +1,4 @@
+
 import Vue from "vue";
 import VueRouter from "vue-router";
 
@@ -7,15 +8,17 @@ const routes = [
     {
         path: '/login',
         name: 'Login',
+        meta: { guess: true },
         component: () => import('../views/Login.vue')
     },
     {
         path: '/register',
         name: 'Regis',
+        meta: { guess: true },
         component: () => import('../views/Regis.vue')
     },
     {
-        path: '/Catalog',
+        path: '/',
         name: 'Catalog',
         component: () => import('../views/Catalog.vue')
     },
@@ -27,26 +30,31 @@ const routes = [
     {
         path: '/Cart',
         name: 'Cart',
+        meta: { member: true },
         component: () => import('../views/Cart.vue')
     },
     {
         path: '/checkout',
         name: 'Checkout',
+        meta: { member: true },
         component: () => import('../views/Checkout.vue')
     },
     {
         path: '/history',
         name: 'History',
+        meta: { member: true },
         component: () => import('../views/History.vue')
     },
     {
         path: '/history/:id',
         name: 'HistoryDetail',
+        meta: { member: true },
         component: () => import('../views/HistoryDetail.vue')
     },
     {
         path: '/Admin',
         name: 'Admin',
+        meta: { admin: "admin" },
         component: () => import('../views/Admin.vue')
     }
 ]
@@ -55,6 +63,28 @@ const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
     routes // short for `routes: routes`
+})
+
+router.beforeEach((to, from, next) => {
+    const isLoggedIn = !!localStorage.getItem('Token')
+    const member = !!localStorage.getItem('Token')
+    const admin = localStorage.getItem('role')
+
+    if (to.meta.login && !isLoggedIn){
+      alert('Please login first!')
+      next({ path: '/login' })
+    } else if (to.meta.guess && isLoggedIn){
+      alert("You've already logged in")
+      next({ path: '/'})
+    } else if (to.meta.member && !member){
+      alert('Please login first!')
+      next({ path: '/login' })
+    } else if (to.meta.admin && admin !== "admin"){
+      alert("You don't have permission")
+      next({ path: '/'})
+    }
+  
+    next()
   })
 
 export default router
